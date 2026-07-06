5af6b69de2cee445c1526d016d0905c24670ae77
AhmedAwadNosir <ahmedalprens98@gmail.com>
2026-07-06 20:38:01 +0300

    second step in data layer create data sources first remote Data source done

---
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,42 @@
+import 'package:bookly/Features/home/data/models/book_modal/book_modal.dart';
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/core/utils/apiServecies/api_services.dart';
+import 'package:bookly/core/utils/app_constants.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}
+
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiServices _apiServices;
+
+  HomeRemoteDataSourceImpl(this._apiServices);
+  @override
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await _apiServices.get(
+        endPoint:
+            "volumes?filter=free-ebooks&q=programing&key=${AppConstants.apiKey}");
+
+    List<BookEntity> books = getBooks(data);
+    return books;
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewestBooks() async {
+    var data = await _apiServices.get(
+        endPoint:
+            "volumes?q=programing&filter=free-ebooks&orderBy=newest&key=${AppConstants.apiKey}");
+
+    List<BookEntity> books = getBooks(data);
+    return books;
+  }
+
+  List<BookEntity> getBooks(Map<String, dynamic> data) {
+    List<BookEntity> books = [];
+    for (var bookMap in data["items"]) {
+      books.add(BookModal.fromJson(bookMap));
+    }
+    return books;
+  }
+}
---
+++ lib/core/utils/app_constants.dart
@@ -0,0 +1,3 @@
+abstract class AppConstants {
+  static const apiKey = "AIzaSyB8lpkMOyaEm863b37riQn47e-Wwc9zQXA";
+}
